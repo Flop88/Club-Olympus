3575636eb245cfa8ba8794d2c3703946ca1c3813
Maksim Likhachev <wwwqwwwqwwwqwwwq@gmail.com>
2020-07-19 19:58:42 +0300

    implements all methods from LoaderManager.LoaderCallbacks

--- app/src/main/java/ru/mvlikhachev/clubolympus/MainActivity.java
+++ app/src/main/java/ru/mvlikhachev/clubolympus/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,6 +21,8 @@ import ru.mvlikhachev.clubolympus.Data.ClubOlympusContract.MemberEntry;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
 
+    public static final int MEMBER_LOADER = 123;
+    MembersCursorAdapter membersCursorAdapter;
     ListView dataListView;
 
     @Override
@@ -38,15 +41,41 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 startActivity(intent);
             }
         });
+
+        membersCursorAdapter = new MembersCursorAdapter(this, null, false);
+        dataListView.setAdapter(membersCursorAdapter);
+        getSupportLoaderManager().initLoader(MEMBER_LOADER,null, this);
     }
 
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        displayData();
+//    }
+
+//    private void displayData() {
+//        String[] projection = {
+//                MemberEntry._ID,
+//                MemberEntry.COLUMN_FIRSTNAME,
+//                MemberEntry.COLUMN_LASTNAME,
+//                MemberEntry.COLUMN_GENDER,
+//                MemberEntry.COLUMN_SPORT,
+//        };
+//        Cursor cursor = getContentResolver().query(
+//                MemberEntry.CONTENT_URI,
+//                projection,
+//                null,
+//                null,
+//                null
+//        );
+//
+//        MembersCursorAdapter cursorAdapter = new MembersCursorAdapter(this, cursor, false);
+//        dataListView.setAdapter(cursorAdapter);
+//    }
+
+    @NonNull
     @Override
-    protected void onStart() {
-        super.onStart();
-        displayData();
-    }
-
-    private void displayData() {
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
         String[] projection = {
                 MemberEntry._ID,
                 MemberEntry.COLUMN_FIRSTNAME,
@@ -54,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 MemberEntry.COLUMN_GENDER,
                 MemberEntry.COLUMN_SPORT,
         };
-        Cursor cursor = getContentResolver().query(
+        CursorLoader cursorLoader = new CursorLoader(this,
                 MemberEntry.CONTENT_URI,
                 projection,
                 null,
@@ -62,23 +91,18 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 null
         );
 
-        MembersCursorAdapter cursorAdapter = new MembersCursorAdapter(this, cursor, false);
-        dataListView.setAdapter(cursorAdapter);
-    }
-
-    @NonNull
-    @Override
-    public Loader onCreateLoader(int id, @Nullable Bundle args) {
-        return null;
+        return cursorLoader;
     }
 
     @Override
     public void onLoadFinished(@NonNull Loader loader, Object data) {
 
+        membersCursorAdapter.swapCursor((Cursor) data);
+
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader loader) {
-
+        membersCursorAdapter.swapCursor(null);
     }
 }
